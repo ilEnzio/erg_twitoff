770f0c2950d719a1a220e5443a8a3e3e859487fc
ilenzio <westfury@gmail.com>
2020-09-16 07:40:45 -0400

    Add Basilica service, added env var for Basilica Key

---
+++ web_app/services/basilica_service.py
@@ -0,0 +1,49 @@
+import basilica
+from basilica import Connection
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")  # TODO use env var
+
+# with basilica.Connection(BASILICA_API_KEY) as c:
+
+#     print("CONNECTION", type(c))
+#     # embeddings = c.embed_sentences(["Hello world!", "How are you?"])
+
+#     sentences = [
+#         'This is a sentence!',
+#         "this is a similar sentence!",
+#         "I don't think this sentence is very similar at all..."
+#     ]
+#     embeddings = c.embed_sentences(sentences)
+
+#     print(list(embeddings))  # [[0.8556405305862427, ...], ...]
+
+#     embedding = c.embed_sentence("Hello World!!!")
+#     breakpoint()
+
+sentences = [
+    'This is a sentence!',
+    "this is a similar sentence!",
+    "I don't think this sentence is very similar at all..."
+]
+connection = Connection(BASILICA_API_KEY)
+
+
+if __name__ == "__main__":
+
+    print("CONNECTION", type(connection))
+
+    embeddings = connection.embed_sentences(sentences)
+
+    print(list(embeddings))  # [[0.8556405305862427, ...], ...]
+
+    embedding = connection.embed_sentence("Hello World!!!", model="twitter")
+    print(type(embedding))  # <class 'list'>
+    print(type(embedding[0]))  # <class "float">
+    print(len(embedding))  # 768
+
+    breakpoint()
